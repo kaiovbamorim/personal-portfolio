86b63b77cac1fb3ef29b896315dca85585881c56
Kaio Amorim <kaiovbamorim@gmail.com>
2023-01-11 16:18:50 -0300

    App JS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Rotas from './Rotas/'
+import Conteudo from './componentes/PaginaInicial/Conteudo';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Rotas />
+    </div>
+  );
+}
+
+export default App;
